deb279a43d9241cc33be638f435ba3101c1f981f
juanmadev5 <juanma261005@gmail.com>
2024-01-15 18:19:42 -0300

    minor changes

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function getRandomColor() {
-    const colors = ['#8a2be2', '#800080', '#E91E63'];
+    const colors = ['#8a2be2', '#800080', '#E91E63', '#3b3030'];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
